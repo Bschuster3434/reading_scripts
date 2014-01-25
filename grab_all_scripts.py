0a44c1bfabaf58447a50e48f47c3cf7746ab3730
Brian Schuster <brian.daniel.schuster@gmail.com>
2014-01-24 19:53:08 -0500

    Created Binaries on CSV for Grab Info

--- grab_all_scripts.py
+++ grab_all_scripts.py
@@ -1,15 +1,16 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
-soup = BeautifulSoup( urllib2.urlopen("http://www.imsdb.com/all%20scripts/" ) )
+def grab_all_scripts():
+	soup = BeautifulSoup( urllib2.urlopen("http://www.imsdb.com/all%20scripts/" ) )
 
-movie_ident = '/Movie Scripts/'
+	movie_ident = '/Movie Scripts/'
 
-links_list = [link.get('href') for link in soup.findAll('a') if link.get('href').find(movie_ident) != -1][5:]
+	links_list = [link.get('href') for link in soup.findAll('a') if link.get('href').find(movie_ident) != -1][5:]
 
-begin_url = "http://www.imsdb.com"
-completed_links = [ begin_url + i.replace(' ', '%20') for i in links_list ]
+	begin_url = "http://www.imsdb.com"
+	completed_links = [ begin_url + i.replace(' ', '%20') for i in links_list ]
 
-with open('script_links.txt', 'w') as file:
-	for link in completed_links:
-		file.write(link + '\n')
+	with open('script_links.txt', 'w') as file:
+		for link in completed_links:
+			file.write(link + '\n')
---
+++ grab_meta_data_and_store.py
@@ -0,0 +1,25 @@
+import csv
+import pandas as pd
+
+def open_meta_links():
+	return pd.read_csv('meta_links_id.csv', names = ['id', 'page_link', 'downloaded_bool'])
+	
+def test_meta():
+	return pd.read_csv('test_meta.csv', names = ['id', 'page_link', 'downloaded_bool'])
+	
+def mark_completed(file, id):
+	new_rows = []
+	
+	with open(file, 'rb') as csv_file:
+		reader = csv.reader(csv_file)
+		for row in reader:
+			if int(row[0]) in id:
+				row[2] = 1
+			new_rows.append(row)
+	
+	with open(file, 'wb') as csv_file2:
+		writer = csv.writer(csv_file2)
+		writer.writerows(new_rows)
+
+
+		
