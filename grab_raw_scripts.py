69b50258fcf3fd13863cf79c50e9d36c59af238d
Brian Schuster <brian.daniel.schuster@gmail.com>
2014-01-29 18:54:41 -0500

    Working on auto-writting scripts to files

--- grab_raw_scripts.py
+++ grab_raw_scripts.py
@@ -5,8 +5,21 @@ import csv
 import itertools
 from random import sample, randint, choice
 import time
+import re
 
-def grab_script(script_url):
+def return_clean_script(script_url):
+	
+	dirty_text = grab_dirty_script(script_url)
+	clean_text = text_cleaner(dirty_text)
+	result = is_text_clean(clean_text)
+	
+	if result == True:
+		return clean_text
+	else:
+		return None
+	
+
+def grab_dirty_script(script_url):
 	"""
 	Grabbing the raw script, one url at a time.
 	"""
@@ -25,6 +38,18 @@ def grab_script(script_url):
 		text_body = soup.findAll('pre')[-1]
 	
 	return text_body
+	
+def is_text_clean(text_body):
+
+	text_body = str(text_body)
+	
+	string_tester = re.compile('<.*>')
+	result = string_tester.match(text_body)
+	
+	if result == None:
+		return True
+	else:
+		return False
 
 def text_cleaner(text):
 	text = str(text)
@@ -34,6 +59,7 @@ def text_cleaner(text):
 	text = text.replace("<b>", "")
 	text = text.replace("</pre", "")
 	text = text.replace("</pre>", "")
+	text = text.replace("<body>", "")
 	return text
 	
 	
@@ -48,3 +74,51 @@ def grab_random_soup():
 	file = choice(df['Raw_Script_URL'])
 	soup = BeautifulSoup( urllib2.urlopen( file ) )
 	return soup
+	
+def grab_specific_script_soup(file):
+	soup = BeautifulSoup( urllib2.urlopen( file ) )
+	return soup
+	
+def text_re():
+	str1 = "<meta charset=\"utf-8\" />"
+	str2 = "This is my testing string"
+	str3 = "<b> I am going to the store </b>"
+	
+	p = re.compile('<.*>')
+	
+	assert p.match(str1) != None
+	assert p.match(str2) == None
+	assert p.match(str3) != None
+	
+	return "Successful Test"
+	
+
+def test_add_scripts():
+	script_file = "test_script.csv"
+	directory = "test_raw_script"
+	
+	s_df = pd.read_csv(script_file)
+	r_row = choice(s_df[ pd.isnull( s_df['Raw_Script_Loc'] ) ].index)
+	
+	script_url = s_df['Raw_Script_URL'].ix[r_row]
+	script_name = s_df['Script_Title'].ix[r_row]
+	
+	c_script = return_clean_script(script_url)
+	
+	if c_script != None:
+		write_file = directory + '/' + script_name + '.txt'
+		with open(write_file, 'w') as w_file:
+			w_file.write(c_script)
+		s_df['Raw_Script_Loc'].ix[r_row] = write_file
+		s_df.to_csv(script_file)
+	else:
+		print "Did not pass clean test"
+		
+			
+		
+		
+	
+	
+	
+	
+	
