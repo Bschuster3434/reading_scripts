66e6eb41cc9c684412dc50d6bf977c65ad8037dc
Brian Schuster <brian.daniel.schuster@gmail.com>
2014-01-31 09:42:40 -0500

    Grabbed all Raw Texts I could

--- grab_raw_scripts.py
+++ grab_raw_scripts.py
@@ -23,7 +23,11 @@ def grab_dirty_script(script_url):
 	"""
 	Grabbing the raw script, one url at a time.
 	"""
-	soup = BeautifulSoup( urllib2.urlopen( script_url ) )
+	try:
+		soup = BeautifulSoup( urllib2.urlopen( script_url ) )
+	except UnicodeEncodeError:
+		print "Unicode Error with " + str(script_url)
+		return "           "
 	
 	#the Body tag might be appropriate, as well as pre
 	#Might be two ways to pull out the script, one using the tag pre, the other body
@@ -116,11 +120,33 @@ def test_add_scripts():
 		s_df['Raw_Script_Loc'].ix[r_row] = "unclean"
 		print "Did not pass clean test"
 		
+def add_scripts():
+	script_file = "script.csv"
+	directory = 'raw_Script'
+	
+	s_df = pd.read_csv(script_file)
+	s_df['Raw_Script_Loc'] = s_df['Raw_Script_Loc'].astype(object)
+	
+	length = len( s_df[ pd.isnull( s_df['Raw_Script_Loc'] ) ].index )
+	
+	for i in range(length):
+	
+		r_row = choice(s_df[ pd.isnull( s_df['Raw_Script_Loc'] ) ].index)
+	
+		script_url = s_df['Raw_Script_URL'].ix[r_row]
+		script_name = s_df['Script_Title'].ix[r_row]
+	
+		c_script = return_clean_script(script_url)
+	
+		if c_script != None:
+			write_file = directory + '/' + script_name + '.txt'
+			with open(write_file, 'w') as w_file:
+				w_file.write(c_script)
+			s_df['Raw_Script_Loc'].ix[r_row] = write_file
+			s_df.to_csv(script_file, index = False)
+		else:
+			s_df['Raw_Script_Loc'].ix[r_row] = "unclean"
+			print "Did not pass clean test"
 			
-		
-		
-	
-	
-	
-	
+		time.sleep(2)
 	
